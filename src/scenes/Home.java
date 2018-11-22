84046f63b5c6a826ce5a0bb8aeb35e592eda1cb2
ThomasPasquali <45013923+ThomasPasquali@users.noreply.github.com>
2018-11-22 11:24:42 +0100

    Add files via upload

--- src/scenes/Home.java
+++ src/scenes/Home.java
@@ -1,7 +1,75 @@
 package scenes;
 
-import javafx.scene.layout.VBox;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Home extends VBox{
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+import misc.Cell;
+
+public class Home extends BorderPane{
+	
+	private List<Cell> cells;
+	
+	public Home() {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("home.fxml"));
+			loader.setController(this);
+			loader.setRoot(this);
+			loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	private GridPane grid;
+	    
+	@FXML
+	void initialize() {
+		int rows=10, cols=10;
+		for(ColumnConstraints col : grid.getColumnConstraints()) {
+			col.setHgrow(Priority.NEVER);
+			col.setHalignment(HPos.CENTER);
+			col.setPrefWidth(-1);
+		}
+		for(RowConstraints row : grid.getRowConstraints()) {
+			row.setVgrow(Priority.NEVER);
+			row.setValignment(VPos.CENTER);
+			row.setPrefHeight(-1);
+		}
+			
+    	cells=new ArrayList<>(rows*cols);
+    	for (int i = 0, numero=1; i < rows; i++) 	
+			for (int j = 0; j < cols; j++) {
+				Cell c=new Cell(i, j, numero, "gray");
+				c.setOnMouseClicked(e->{
+					System.out.println(c.getNumber());
+					c.setColor(c.getColor().equals("green")?"red":"green");
+				});
+				cells.add(c);
+				grid.add(c, j, i);
+				numero++;
+			}
+		
+	}
+	
+	@FXML
+    void book() {
+
+    }
+
+    @FXML
+    void del() {
+
+    }
 	
 }
