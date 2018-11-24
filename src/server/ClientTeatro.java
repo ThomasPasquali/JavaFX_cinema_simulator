cb26555d41bb56ae2599c1e7624aaac09ffd4dec
ThomasPasquali <45013923+ThomasPasquali@users.noreply.github.com>
2018-11-24 10:37:17 +0100

    Add files via upload

--- src/server/ClientTeatro.java
+++ src/server/ClientTeatro.java
@@ -2,6 +2,9 @@ package server;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.StringJoiner;
 
 import misc.Client;
 
@@ -9,10 +12,14 @@ public class ClientTeatro extends Thread{
 
 	private Client client;
 	private Connection connection;
+	private ServerTeatro server;
+	private int id;
 
-	public ClientTeatro(Client client, Connection connection) {
+	public ClientTeatro(Client client, int id, Connection connection, ServerTeatro server) {
 		this.client = client;
 		this.connection=connection;
+		this.id=id;
+		this.server=server;
 		start();
 	}
 	
@@ -22,12 +29,53 @@ public class ClientTeatro extends Thread{
 		try {
 			while(!(request=client.readLine()).equals("QUIT")){
 				String[] params=request.split(" ");
-				if(params[0].equals("GET_ACCOUNT")){
+				if(params[0].equals("GET_SHOWS")){
+					Statement s=connection.createStatement();
+					ResultSet set=s.executeQuery(
+							"SELECT  s.ID AS id,  s.Titolo AS Titolo , \r\n" + 
+							"			s.Ora AS Ora,  \r\n" + 
+							"			sala.Posti -\r\n" + 
+							"			(SELECT COUNT(*)\r\n" + 
+							"			 FROM prenotazioni AS p\r\n" + 
+							"			 WHERE p.Spettacolo = s.ID)AS Liberi, \r\n" + 
+							"			sala.Posti AS Totali, \r\n" + 
+							"			sala.Nome AS Hall\r\n" + 
+							"FROM spettacoli AS s\r\n" + 
+							"	JOIN sale AS sala ON s.Sala =  sala.ID\r\n" + 
+							"WHERE s.Data = '"+params[1]+"'");
+					StringJoiner sjAnd=new StringJoiner("&");
+					while(set.next()) {
+						StringJoiner sj=new StringJoiner("/");
+						sj.add(set.getString("id"));
+						sj.add(set.getString("Titolo"));
+						sj.add(params[1]);
+						sj.add(set.getString("Ora"));
+						sj.add(set.getString("Liberi"));
+						sj.add(set.getString("Totali"));
+						sj.add(set.getString("Hall"));
+						sjAnd.add(sj.toString());
+					}
+					if(sjAnd.toString().isEmpty()) {
+						System.out.println("non trovato");
+						client.write("NO_DATA");
+					}
+						
+					else {
+						System.out.println("trovato");
+							client.write("SHOWS_FOR_"+params[1]+" "+sjAnd.toString());
+					}
 					
 				}
 			}
-		} catch (IOException e) {
+		} catch (Exception e) {
+			server.removeClient(id);
+			try {
+				client.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 			e.printStackTrace();
+			System.out.println(id+" LEFT");
 		}
 	}
 	
