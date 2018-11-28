4e55738532f7dceac3044e6188204b1af1bb8b2e
ThomasPasquali <45013923+ThomasPasquali@users.noreply.github.com>
2018-11-28 12:32:14 +0100

    Add files via upload

--- src/misc/Show.java
+++ src/misc/Show.java
@@ -4,17 +4,16 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-import main.Main;
-
 public class Show {
 	
 	private static DateTimeFormatter timeF=DateTimeFormatter.ofPattern("HH:mm");
 	private String title, hall;
 	private LocalDate date;
 	private LocalTime time;
-	private int free, tot;
+	private int free, tot, id;
 	
-	public Show(String title, LocalDate date, LocalTime time, int free, int tot, String hall) {
+	public Show(int id, String title, LocalDate date, LocalTime time, int free, int tot, String hall) {
+		this.id=id;
 		this.title = title;
 		this.date = date;
 		this.time = time;
@@ -22,9 +21,37 @@ public class Show {
 		this.tot = tot;
 		this.hall = hall;
 	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getHall() {
+		return hall;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public int getFree() {
+		return free;
+	}
+
+	public int getTot() {
+		return tot;
+	}
 	
+	public int getId() {
+		return id;
+	}
+
 	@Override
 	public String toString() {
-		return title+" Time: "+timeF.format(time)+" Hall: "+hall+" Free seats: "+free+"/"+tot;
+		return "Titolo: " + title + "\r\nSala: " + hall + "\r\nData: " + date + " " + timeF.format(time) + "\r\nPosti:" + free+ "/" + tot;
 	}
 }
